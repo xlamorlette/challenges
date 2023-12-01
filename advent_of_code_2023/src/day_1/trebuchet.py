b2207f3df7d0e1faac7ea903e8dc6f4116190f43
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-01 21:03:26 +0100

    advent of code 2023: day 1 - part 2

--- advent_of_code_2023/src/day_1/trebuchet.py
+++ advent_of_code_2023/src/day_1/trebuchet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import Dict, List, Optional
 
 
 def read_file(calibration_file: str) -> List[str]:
@@ -11,16 +11,48 @@ def read_file(calibration_file: str) -> List[str]:
     return data
 
 
+SPELLED_DIGITS: Dict[str, int] = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def get_spelled_digit(line: str,
+                      go_backward: bool = False) -> Optional[int]:
+    for character_index in range(len(line)):
+        substring = line[len(line) - character_index - 1:] if go_backward else line[character_index:]
+        for spelled_digit, digit in SPELLED_DIGITS.items():
+            if substring[:len(spelled_digit)] == spelled_digit:
+                return digit
+    return None
+
+
 def get_first_digit(line: str) -> int:
-    for character in line:
+    for character_index, character in enumerate(line):
         if character.isdigit():
+            line_begining = line[:character_index]
+            digit = get_spelled_digit(line_begining)
+            if digit:
+                return digit
             return int(character)
     raise RuntimeError("no digit found")
 
 
 def get_last_digit(line: str) -> int:
-    for character in reversed(line):
+    for character_index in range(len(line) - 1, -1, -1):
+        character = line[character_index]
         if character.isdigit():
+            line_ending = line[character_index + 1:]
+            digit = get_spelled_digit(line_ending, go_backward=True)
+            if digit:
+                return digit
             return int(character)
     raise RuntimeError("no digit found")
 
--- advent_of_code_2023/src/day_1/trebuchet_test.py
+++ advent_of_code_2023/src/day_1/trebuchet_test.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.day_1.trebuchet import compute_calibration_sum, get_calibration_value, get_first_digit, get_last_digit
+from src.day_1.trebuchet import compute_calibration_sum, get_calibration_value, get_first_digit, get_last_digit, \
+    get_spelled_digit
 
 
 def test_compute_calibration_sum():
@@ -15,6 +16,16 @@ def test_get_calibration_value():
     assert get_calibration_value("treb7uchet") == 77
 
 
+def test_get_calibration_value_with_spelled_digits():
+    assert get_calibration_value("two1nine") == 29
+    assert get_calibration_value("abcone2threexyz") == 13
+    assert get_calibration_value("xtwone3four") == 24
+    assert get_calibration_value("4nineeightseven2") == 42
+    assert get_calibration_value("zoneight234") == 14
+    assert get_calibration_value("7pqrstsixteen") == 76
+    assert get_calibration_value("1twone") == 11
+
+
 def test_get_first_digit():
     assert get_first_digit("1abc2") == 1
     assert get_first_digit("pqr3stu8vwx") == 3
@@ -26,6 +37,15 @@ def test_get_first_digit():
         get_first_digit("foo")
 
 
+def test_get_first_digit_with_spelled_digits():
+    assert get_first_digit("two1nine") == 2
+    assert get_first_digit("abcone2threexyz") == 1
+    assert get_first_digit("xtwone3four") == 2
+    assert get_first_digit("4nineeightseven2") == 4
+    assert get_first_digit("zoneight234") == 1
+    assert get_first_digit("7pqrstsixteen") == 7
+
+
 def test_get_last_digit():
     assert get_last_digit("1abc2") == 2
     assert get_last_digit("pqr3stu8vwx") == 8
@@ -35,3 +55,34 @@ def test_get_last_digit():
         get_last_digit("")
     with pytest.raises(RuntimeError, match="no digit found"):
         get_last_digit("foo")
+
+
+def test_get_last_digit_with_spelled_digits():
+    assert get_last_digit("two1nine") == 9
+    assert get_last_digit("abcone2threexyz") == 3
+    assert get_last_digit("xtwone3four") == 4
+    assert get_last_digit("4nineeightseven2") == 2
+    assert get_last_digit("zoneight234") == 4
+    assert get_last_digit("7pqrstsixteen") == 6
+    assert get_last_digit("1twone") == 1
+
+
+def test_get_spelled_digit():
+    assert get_spelled_digit("") is None
+    assert get_spelled_digit("foo") is None
+    assert get_spelled_digit("one") == 1
+    assert get_spelled_digit("atwo") == 2
+    assert get_spelled_digit("abthree") == 3
+    assert get_spelled_digit("foura") == 4
+    assert get_spelled_digit("fiveab") == 5
+    assert get_spelled_digit("asixb") == 6
+    assert get_spelled_digit("seveneight") == 7
+    assert get_spelled_digit("eight") == 8
+    assert get_spelled_digit("nineight") == 9
+
+
+def test_get_spelled_digit_reversed():
+    assert get_spelled_digit("one", go_backward=True) == 1
+    assert get_spelled_digit("atwo", go_backward=True) == 2
+    assert get_spelled_digit("threefoo", go_backward=True) == 3
+    assert get_spelled_digit("twoone", go_backward=True) == 1
