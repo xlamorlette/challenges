216a395f1c56a865fc31f4f66104e596b5d9330f
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-01 20:04:47 +0100

    advent of code 2023: day 1 - part 1

--- advent_of_code_2023/src/day_1/trebuchet.py
+++ advent_of_code_2023/src/day_1/trebuchet.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
-
 from typing import List
 
 
-def read_file(_calibration_file: str) -> List[str]:
-    return []
+def read_file(calibration_file: str) -> List[str]:
+    data: List[str] = []
+    with open(calibration_file, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.rstrip()
+            data.append(line)
+    return data
 
 
-def compute_calibration_sum(_calibration_data: List[str]) -> int:
-    return 0
+def get_first_digit(line: str) -> int:
+    for character in line:
+        if character.isdigit():
+            return int(character)
+    raise RuntimeError("no digit found")
+
+
+def get_last_digit(line: str) -> int:
+    for character in reversed(line):
+        if character.isdigit():
+            return int(character)
+    raise RuntimeError("no digit found")
+
+
+def get_calibration_value(line: str) -> int:
+    first_digit: int = get_first_digit(line)
+    last_digit: int = get_last_digit(line)
+    return first_digit * 10 + last_digit
+
+
+def compute_calibration_sum(calibration_data: List[str]) -> int:
+    return sum(get_calibration_value(line) for line in calibration_data)
 
 
 if __name__ == "__main__":
-    calibration_data = read_file("input.txt")
-    result = compute_calibration_sum(calibration_data)
+    file_data = read_file("input.txt")
+    result = compute_calibration_sum(file_data)
     print(f"Sum of the calibration values: {result}")
--- advent_of_code_2023/src/day_1/trebuchet_test.py
+++ advent_of_code_2023/src/day_1/trebuchet_test.py
@@ -1,5 +1,37 @@
-from src.day_1.trebuchet import compute_calibration_sum
+import pytest
+
+from src.day_1.trebuchet import compute_calibration_sum, get_calibration_value, get_first_digit, get_last_digit
 
 
 def test_compute_calibration_sum():
     assert compute_calibration_sum([]) == 0
+    assert compute_calibration_sum(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
+
+
+def test_get_calibration_value():
+    assert get_calibration_value("1abc2") == 12
+    assert get_calibration_value("pqr3stu8vwx") == 38
+    assert get_calibration_value("a1b2c3d4e5f") == 15
+    assert get_calibration_value("treb7uchet") == 77
+
+
+def test_get_first_digit():
+    assert get_first_digit("1abc2") == 1
+    assert get_first_digit("pqr3stu8vwx") == 3
+    assert get_first_digit("a1b2c3d4e5f") == 1
+    assert get_first_digit("treb7uchet") == 7
+    with pytest.raises(RuntimeError, match="no digit found"):
+        get_first_digit("")
+    with pytest.raises(RuntimeError, match="no digit found"):
+        get_first_digit("foo")
+
+
+def test_get_last_digit():
+    assert get_last_digit("1abc2") == 2
+    assert get_last_digit("pqr3stu8vwx") == 8
+    assert get_last_digit("a1b2c3d4e5f") == 5
+    assert get_last_digit("treb7uchet") == 7
+    with pytest.raises(RuntimeError, match="no digit found"):
+        get_last_digit("")
+    with pytest.raises(RuntimeError, match="no digit found"):
+        get_last_digit("foo")
