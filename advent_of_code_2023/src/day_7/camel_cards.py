51277febb9c132fe2ba2d374f8e38a5d3076a755
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-07 21:54:26 +0100

    advent of code 2023: day 7 - part 2

--- advent_of_code_2023/src/day_7/camel_cards.py
+++ advent_of_code_2023/src/day_7/camel_cards.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from enum import IntEnum
-from typing import Dict, Final, List
+from typing import Final, List
 
 
 class HandType(IntEnum):
@@ -13,64 +13,61 @@ class HandType(IntEnum):
     HIGH_CARD = 1
 
 
-VALUE_PER_CARD: Final[Dict[str, int]] = {
-    "A": 14,
-    "K": 13,
-    "Q": 12,
-    "J": 11,
-    "T": 10,
-    "9": 9,
-    "8": 8,
-    "7": 7,
-    "6": 6,
-    "5": 5,
-    "4": 4,
-    "3": 3,
-    "2": 2
-}
+CARDS_BY_INCREASING_VALUE: Final[str] = "J23456789TQKA"
+
+
+def get_card_value(card: str) -> int:
+    return CARDS_BY_INCREASING_VALUE.index(card) + 1
 
 
 class Hand:
     bid: int
-    cards: List[str]
+    cards: str
     type: HandType
     value: int
 
     def __init__(self,
                  line: str):
         line_items = line.split()
-        self.cards = list(line_items[0])
+        self.cards = line_items[0]
         self.bid = int(line_items[1])
-        self._determine_type()
+        self.type = self._compute_type(self.cards)
         self._compute_value()
 
-    def _determine_type(self):
-        number_per_card_in_hand = Counter(self.cards)
-        card_cardinals = list(number_per_card_in_hand.values())
-        card_cardinals.sort(reverse=True)
-        if card_cardinals[0] == 5:
-            self.type = HandType.FIVE_OF_A_KIND
-        elif card_cardinals[0] == 4:
-            self.type = HandType.FOUR_OF_A_KIND
-        elif card_cardinals[0] == 3 and card_cardinals[1] == 2:
-            self.type = HandType.FULL_HOUSE
-        elif card_cardinals[0] == 3:
-            self.type = HandType.THREE_OF_KIND
-        elif card_cardinals[0] == 2 and card_cardinals[1] == 2:
-            self.type = HandType.TWO_PAIRS
-        elif card_cardinals[0] == 2:
-            self.type = HandType.ONE_PAIR
-        else:
-            self.type = HandType.HIGH_CARD
+    @staticmethod
+    def _compute_type(cards: str) -> HandType:
+        number_of_jokers = cards.count("J")
+        if number_of_jokers == 5:
+            return HandType.FIVE_OF_A_KIND
+        cards_without_jokers = list(filter(lambda card: card != "J", cards))
+        number_per_card_in_hand = Counter(cards_without_jokers)
+        card_cardinals = sorted(list(number_per_card_in_hand.values()), reverse=True)
+        # the jokers add up to the most frequent card
+        card_cardinals[0] += number_of_jokers
+        match card_cardinals[0]:
+            case 5:
+                return HandType.FIVE_OF_A_KIND
+            case 4:
+                return HandType.FOUR_OF_A_KIND
+            case 3:
+                if card_cardinals[1] == 2:
+                    return HandType.FULL_HOUSE
+                return HandType.THREE_OF_KIND
+            case 2:
+                if card_cardinals[1] == 2:
+                    return HandType.TWO_PAIRS
+                return HandType.ONE_PAIR
+            case _other:
+                return HandType.HIGH_CARD
 
     def _compute_value(self):
         value_string: str = str(int(self.type))
         for card in self.cards:
-            value_string += f"{VALUE_PER_CARD[card]:02d}"
+            value_string += f"{get_card_value(card):02d}"
         self.value = int(value_string)
 
     def __repr__(self):
-        return f"({''.join(self.cards)}, {self.bid}, {self.value})"
+        return f"({self.cards}, {self.bid}, {self.value})"
 
 
 def compute_total_winnings(lines: List[str]) -> int:
--- advent_of_code_2023/src/day_7/camel_cards_test.py
+++ advent_of_code_2023/src/day_7/camel_cards_test.py
@@ -1,6 +1,6 @@
 from typing import Final, List
 
-from src.day_7.camel_cards import Hand, HandType, compute_total_winnings
+from src.day_7.camel_cards import Hand, HandType, compute_total_winnings, get_card_value
 
 INPUT: Final[str] = """32T3K 765
 T55J5 684
@@ -12,7 +12,7 @@ INPUT_LINES: Final[List[str]] = INPUT.split("\n")
 
 
 def test_compute_total_winnings():
-    assert compute_total_winnings(INPUT_LINES) == 6440
+    assert compute_total_winnings(INPUT_LINES) == 5905
 
 
 def test_hand_init():
@@ -21,21 +21,43 @@ def test_hand_init():
     assert hand.bid == 765
 
 
-def test_hand_determine_type():
-    assert Hand("23456 765").type == HandType.HIGH_CARD
-    assert Hand("32T3K 765").type == HandType.ONE_PAIR
-    assert Hand("KK677 28").type == HandType.TWO_PAIRS
-    assert Hand("T55J5 684").type == HandType.THREE_OF_KIND
-    assert Hand("QJQQJ 1").type == HandType.FULL_HOUSE
+def test_hand_compute_type():
+    assert Hand("23456 1").type == HandType.HIGH_CARD
+    assert Hand("32T3K 1").type == HandType.ONE_PAIR
+    assert Hand("KK677 1").type == HandType.TWO_PAIRS
+    assert Hand("T5535 1").type == HandType.THREE_OF_KIND
+    assert Hand("Q3QQ3 1").type == HandType.FULL_HOUSE
     assert Hand("32222 1").type == HandType.FOUR_OF_A_KIND
     assert Hand("22222 1").type == HandType.FIVE_OF_A_KIND
 
 
+def test_hand_compute_type_with_jokers():
+    assert Hand("23J56 1").type == HandType.ONE_PAIR
+    assert Hand("22J56 1").type == HandType.THREE_OF_KIND
+    assert Hand("22J33 1").type == HandType.FULL_HOUSE
+    assert Hand("22J23 1").type == HandType.FOUR_OF_A_KIND
+    assert Hand("22J22 1").type == HandType.FIVE_OF_A_KIND
+    assert Hand("2JJ56 1").type == HandType.THREE_OF_KIND
+    assert Hand("2JJ26 1").type == HandType.FOUR_OF_A_KIND
+    assert Hand("2JJ22 1").type == HandType.FIVE_OF_A_KIND
+    assert Hand("2JJJ6 1").type == HandType.FOUR_OF_A_KIND
+    assert Hand("2JJ2J 1").type == HandType.FIVE_OF_A_KIND
+    assert Hand("2JJJJ 1").type == HandType.FIVE_OF_A_KIND
+    assert Hand("JJJJJ 1").type == HandType.FIVE_OF_A_KIND
+
+
 def test_hand_compute_value():
-    assert Hand("32T3K 1").value == 20302100313
+    assert Hand("32T3K 1").value == 20302100312
     assert Hand("23456 1").value == 10203040506
-    assert Hand("KK677 1").value == 31313060707
-    assert Hand("Q55J5 1").value == 41205051105
-    assert Hand("QAQQA 1").value == 51214121214
+    assert Hand("KK677 1").value == 31212060707
+    assert Hand("Q5535 1").value == 41105050305
+    assert Hand("QAQQA 1").value == 51113111113
     assert Hand("32222 1").value == 60302020202
     assert Hand("22222 1").value == 70202020202
+
+
+def test_get_card_value():
+    assert get_card_value("J") == 1
+    assert get_card_value("2") == 2
+    assert get_card_value("T") == 10
+    assert get_card_value("A") == 13
