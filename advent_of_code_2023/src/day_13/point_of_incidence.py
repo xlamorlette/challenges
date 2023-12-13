819a376ba945095703ac1967144c5db28cd41a33
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-13 23:00:53 +0100

    advent of code 2023: day 13: first improvement of code

--- advent_of_code_2023/src/day_13/point_of_incidence.py
+++ advent_of_code_2023/src/day_13/point_of_incidence.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 
 
 def compute_sum_of_reflection_numbers(lines: List[str]) -> int:
-    return sum(map(get_reflection_number, get_patterns(lines)))
+    return sum(get_reflection_number(line) or 0 for line in get_patterns(lines))
 
 
 def get_patterns(lines: List[str]) -> List[List[str]]:
@@ -14,14 +14,12 @@ def get_patterns(lines: List[str]) -> List[List[str]]:
 
 def get_reflection_number(lines: List[str],
                           excluded_vertical_axis: Optional[int] = None,
-                          excluded_horizontal_axis: Optional[int] = None) -> int:
-    vertical_symmetry_axis: Optional[int] = get_vertical_symmetry_axis(lines, excluded_vertical_axis)
-    if vertical_symmetry_axis:
+                          excluded_horizontal_axis: Optional[int] = None) -> Optional[int]:
+    if vertical_symmetry_axis := get_vertical_symmetry_axis(lines, excluded_vertical_axis):
         return vertical_symmetry_axis
-    horizontal_symmetry_axis: Optional[int] = get_horizontal_symmetry_axis(lines, excluded_horizontal_axis)
-    if horizontal_symmetry_axis:
+    if horizontal_symmetry_axis := get_horizontal_symmetry_axis(lines, excluded_horizontal_axis):
         return horizontal_symmetry_axis * 100
-    return 0
+    return None
 
 
 def get_vertical_symmetry_axis(lines: List[str],
@@ -42,10 +40,11 @@ def get_horizontal_symmetry_axis(lines: List[str],
 
 def is_horizontal_symmetry_axis(lines: List[str],
                                 row_axis: int) -> bool:
-    symmetric_size = min(row_axis, len(lines) - row_axis)
-    upper_part: List[str] = lines[row_axis - symmetric_size: row_axis]
-    lower_part: List[str] = lines[row_axis: row_axis + symmetric_size]
-    return upper_part == list(reversed(lower_part))
+    upper_part = list(reversed(lines[:row_axis]))
+    lower_part = lines[row_axis:]
+    upper_part = upper_part[:len(lower_part)]
+    lower_part = lower_part[:len(upper_part)]
+    return upper_part == lower_part
 
 
 def compute_sum_of_smudged_reflection_numbers(lines: List[str]) -> int:
@@ -53,16 +52,15 @@ def compute_sum_of_smudged_reflection_numbers(lines: List[str]) -> int:
 
 
 def get_smudged_reflection_number(lines: List[str]) -> int:
-    reflection_number = get_reflection_number(lines)
+    reflection_number = get_reflection_number(lines) or 0
     excluded_vertical_axis: Optional[int] = reflection_number if reflection_number < 100 else None
     excluded_horizontal_axis: Optional[int] = int(reflection_number / 100) if reflection_number >= 100 else None
     for row, line in enumerate(lines):
         for column in range(len(lines[0])):
             fixed_line = line[:column] + inverted_character(line[column]) + line[column + 1:]
             fixed_lines = lines[:row] + [fixed_line] + lines[row + 1:]
-            smudged_reflection_number = get_reflection_number(fixed_lines, excluded_vertical_axis,
-                                                              excluded_horizontal_axis)
-            if smudged_reflection_number != 0:
+            if smudged_reflection_number := get_reflection_number(fixed_lines, excluded_vertical_axis,
+                                                                  excluded_horizontal_axis):
                 return smudged_reflection_number
     return 0
 
