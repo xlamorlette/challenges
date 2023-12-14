c67fce4dc24d28dcceabd5919b139af5a98fb5c4
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-14 19:11:25 +0100

    advent of code 2023: day 13: second improvement of code

--- advent_of_code_2023/src/day_13/point_of_incidence.py
+++ advent_of_code_2023/src/day_13/point_of_incidence.py
@@ -13,38 +13,37 @@ def get_patterns(lines: List[str]) -> List[List[str]]:
 
 
 def get_reflection_number(lines: List[str],
-                          excluded_vertical_axis: Optional[int] = None,
-                          excluded_horizontal_axis: Optional[int] = None) -> Optional[int]:
-    if vertical_symmetry_axis := get_vertical_symmetry_axis(lines, excluded_vertical_axis):
+                          target_differences_count: int = 0) -> int:
+    if vertical_symmetry_axis := get_vertical_symmetry_axis(lines, target_differences_count):
         return vertical_symmetry_axis
-    if horizontal_symmetry_axis := get_horizontal_symmetry_axis(lines, excluded_horizontal_axis):
+    if horizontal_symmetry_axis := get_horizontal_symmetry_axis(lines, target_differences_count):
         return horizontal_symmetry_axis * 100
-    return None
+    assert False, "no reflection found"
 
 
 def get_vertical_symmetry_axis(lines: List[str],
-                               excluded_axis: Optional[int] = None) -> Optional[int]:
+                               target_differences_count: int = 0) -> Optional[int]:
     columns: List[str] = list(map("".join, zip(*lines)))
-    return get_horizontal_symmetry_axis(columns, excluded_axis)
+    return get_horizontal_symmetry_axis(columns, target_differences_count)
 
 
 def get_horizontal_symmetry_axis(lines: List[str],
-                                 excluded_axis: Optional[int] = None) -> Optional[int]:
+                                 allowed_differences_count: int = 0) -> Optional[int]:
     for row in range(1, len(lines)):
-        if row == excluded_axis:
-            continue
-        if is_horizontal_symmetry_axis(lines, row):
+        if is_horizontal_symmetry_axis(lines, row, allowed_differences_count):
             return row
     return None
 
 
 def is_horizontal_symmetry_axis(lines: List[str],
-                                row_axis: int) -> bool:
+                                row_axis: int,
+                                target_differences_count: int = 0) -> bool:
     upper_part = list(reversed(lines[:row_axis]))
     lower_part = lines[row_axis:]
-    upper_part = upper_part[:len(lower_part)]
-    lower_part = lower_part[:len(upper_part)]
-    return upper_part == lower_part
+    differences = sum(sum(character_upper != character_lower
+                          for character_upper, character_lower in zip(line_upper, line_lower))
+                      for line_upper, line_lower in zip(upper_part, lower_part))
+    return differences == target_differences_count
 
 
 def compute_sum_of_smudged_reflection_numbers(lines: List[str]) -> int:
@@ -52,18 +51,4 @@ def compute_sum_of_smudged_reflection_numbers(lines: List[str]) -> int:
 
 
 def get_smudged_reflection_number(lines: List[str]) -> int:
-    reflection_number = get_reflection_number(lines) or 0
-    excluded_vertical_axis: Optional[int] = reflection_number if reflection_number < 100 else None
-    excluded_horizontal_axis: Optional[int] = int(reflection_number / 100) if reflection_number >= 100 else None
-    for row, line in enumerate(lines):
-        for column in range(len(lines[0])):
-            fixed_line = line[:column] + inverted_character(line[column]) + line[column + 1:]
-            fixed_lines = lines[:row] + [fixed_line] + lines[row + 1:]
-            if smudged_reflection_number := get_reflection_number(fixed_lines, excluded_vertical_axis,
-                                                                  excluded_horizontal_axis):
-                return smudged_reflection_number
-    return 0
-
-
-def inverted_character(character: str) -> str:
-    return "." if character == "#" else "#"
+    return get_reflection_number(lines, 1)
