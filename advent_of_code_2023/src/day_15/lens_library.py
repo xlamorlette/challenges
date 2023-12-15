1a7aef4f11c0f6a69e4df6d709f67778e7cc21ca
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2023-12-15 19:38:47 +0100

    advent of code 2023: day 15 - part 1: code improvement

--- advent_of_code_2023/src/day_15/lens_library.py
+++ advent_of_code_2023/src/day_15/lens_library.py
@@ -1,11 +1,9 @@
+import functools
+
+
 def compute_sum_of_hashes(line: str) -> int:
     return sum(map(get_hash, line.split(',')))
 
 
 def get_hash(step: str) -> int:
-    value: int = 0
-    for character in step:
-        value += ord(character)
-        value *= 17
-        value = value % 256
-    return value
+    return functools.reduce(lambda value, character: ((value + ord(character)) * 17) % 256, step, 0)
