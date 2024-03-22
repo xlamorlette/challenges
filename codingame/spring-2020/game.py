5fda58df9e671f34ea0a92e719616bfe7b3f2e08
Xavier Lamorlette <xavier.lamorlette@gmail.com>
2024-03-22 23:25:31 +0100

    CodinGame: Spring 2020: run away from opponent pac

--- codingame/spring-2020/game.py
+++ codingame/spring-2020/game.py
@@ -3,24 +3,36 @@
 # 1h10 : Bronze 822
 # 2h : Bronze 815
 # 2h30 : Bronze 483
-# 2h40 : Bronze 246
 # 2h50 : Bronze 211
+# 4h30 : Bronze 221
 
 # TODO:
-# run away from opponent pac if too close
 # Maintain grid step:
+#   cell state: empty / unknown / filled
 #   start: all cells are unknown
 #   each turn:
-#     clear cells seen by pac
+#     set all filled cells to unknown
+#     set all cells seen by pacs to empty
 #     fill cells from input
 # For each pac, test 4 directions.
+#     Handle moves across limits.
 #   Evaluate each resulting position:
-#     Flood
-#       Handle moves across limits (also for testing 4 directions)
-#     Value of pellet divided by distance
-#     Value for unknown cell
-#     Value for opponent pac that can be eaten
-# Handle turn with speed only: keep in mind previously targeted pellets
+#     Flood full map
+#     while flooding, add cell value divided by distance
+#       unknown cell: value 0.5
+# Avoid auto-blocking:
+#   simulate one turn move
+#   take randomly one pac and make it moving opposite direction
+# Try to chase unseen pacs:
+#   Value for opponent pac that can be eaten
+#   Negative value for opponent pac that can eat if no ability to switch
+#   add to opponent pac number of turn since last seen
+#   keep seen opponent pacs one turn (or two turns)
+# don't attack opponent pac that can switch
+# attack by switching at the last time
+# Handle turn with speed only:
+#   What is exactly the input?
+#   keep in mind previously targeted pellets
 
 
 from __future__ import annotations
@@ -36,33 +48,43 @@ def debug(message: str):
 
 @dataclass(frozen=True, order=True)
 class Position:
-    row: int
-    column: int
+    x: int
+    y: int
 
     def __add__(self,
                 other: Position) -> Position:
-        return Position(self.row + other.row, self.column + other.column)
+        return Position(self.x + other.x, self.y + other.y)
+
+    def __sub__(self,
+                other: Position) -> Position:
+        return Position(self.x - other.x, self.y - other.y)
 
     def __mul__(self,
                 factor: int) -> Position:
-        return Position(self.row * factor, self.column * factor)
+        return Position(self.x * factor, self.y * factor)
 
     def __repr__(self):
-        return f"({self.row}, {self.column})"
+        return f"({self.x}, {self.y})"
 
     def manhattan_distance(self,
                            other: Position) -> int:
-        return abs(self.row - other.row) + abs(self.column - other.column)
+        return abs(self.x - other.x) + abs(self.y - other.y)
 
     def opposite(self) -> Position:
-        return Position(- self.row, - self.column)
+        return Position(- self.x, - self.y)
+
+    def crop_to_limits(self,
+                       max_position: Position) -> Position:
+        x = min(max(0, self.x), max_position.x)
+        y = min(max(0, self.y), max_position.y)
+        return Position(x=x, y=y)
 
 
 NIL_POSITION: Final[Position] = Position(0, 0)
-NORTH: Final[Position] = Position(-1, 0)
-EAST: Final[Position] = Position(0, 1)
-SOUTH: Final[Position] = Position(1, 0)
-WEST: Final[Position] = Position(0, -1)
+NORTH: Final[Position] = Position(0, -1)
+EAST: Final[Position] = Position(1, 0)
+SOUTH: Final[Position] = Position(0, 1)
+WEST: Final[Position] = Position(-1, 0)
 ALL_DIRECTIONS: Final[list[Position]] = [NORTH, EAST, SOUTH, WEST]
 
 
@@ -72,13 +94,14 @@ class Grid:
     rows: list[str]
 
     def init_from_input(self):
-        # width: size of the grid
-        # height: top left corner is (x=0, y=0)
         self.width, self.height = [int(i) for i in input().split()]
         self.rows = []
         for i in range(self.height):
             row = input()  # one line of the grid: space " " is floor, pound "#" is wall
             self.rows.append(row)
+
+    def get_max_position(self) -> Position:
+        return Position(x=self.width - 1, y=self.height - 1)
 
 
 @dataclass(frozen=True, order=True)
@@ -95,15 +118,15 @@ class Pac:
         inputs = input().split()
         pac_id = int(inputs[0])  # pac number (unique within a team)
         mine = inputs[1] != "0"  # true if this pac is yours
-        row = int(inputs[2])  # position in the grid
-        column = int(inputs[3])  # position in the grid
-        type_id = inputs[4]  # unused in wood leagues
-        speed_turns_left = int(inputs[5])  # unused in wood leagues
-        ability_cooldown = int(inputs[6])  # unused in wood leagues
+        x = int(inputs[2])
+        y = int(inputs[3])
+        type_id = inputs[4]
+        speed_turns_left = int(inputs[5])
+        ability_cooldown = int(inputs[6])
         return Pac(
             pac_id=pac_id,
             mine=mine,
-            position=Position(row, column),
+            position=Position(x=x, y=y),
             type_id=type_id,
             speed_turns_left=speed_turns_left,
             ability_cooldown=ability_cooldown
@@ -140,8 +163,8 @@ class Pellet:
 
     @staticmethod
     def get_pellet_from_input() -> Pellet:
-        row, column, value = [int(j) for j in input().split()]
-        return Pellet(position=Position(row, column), value=value)
+        x, y, value = [int(j) for j in input().split()]
+        return Pellet(position=Position(x=x, y=y), value=value)
 
 
 class State:
@@ -203,7 +226,7 @@ class Solver:
         for opponent_pac in self.state.pac_list:
             if opponent_pac.mine:
                 continue
-            if opponent_pac.position.manhattan_distance(pac.position) > 5:
+            if opponent_pac.position.manhattan_distance(pac.position) > 6:
                 continue
             if opponent_pac.does_beat(pac):
                 target.defend_pac = opponent_pac
@@ -249,27 +272,44 @@ class Solver:
                                target_per_pac: dict[Pac, Target]) -> list[str]:
         return [self.get_pac_action(pac, target) for pac, target in target_per_pac.items()]
 
-    @staticmethod
-    def get_pac_action(pac: Pac,
+    def get_pac_action(self,
+                       pac: Pac,
                        target: Target) -> str:
         if target.defend_pac is not None:
-            if pac.ability_cooldown == 0:
-                new_type_id: str = target.defend_pac.get_beating_type()
-                return f"SWITCH {pac.pac_id} {new_type_id} switch to defend against {target.defend_pac.pac_id}"
-            # TODO: run away
+            action: Optional[str] = self.get_defend_action(pac, target.defend_pac)
+            if action is not None:
+                return action
         if target.attack_pac is not None:
-            attack_pac: Pac = target.attack_pac
-            if pac.position.manhattan_distance(attack_pac.position) <= 1 \
-                    or pac.speed_turns_left > 0 \
-                    or pac.ability_cooldown > 0:
-                return f"MOVE {pac.pac_id} {attack_pac.position.row} {attack_pac.position.column}" \
-                       + f" attack {attack_pac.pac_id}"
-            if pac.ability_cooldown == 0:
-                return f"SPEED {pac.pac_id} rush to attack {attack_pac.pac_id}"
+            action = self.get_attack_action(pac, target.attack_pac)
+            if action is not None:
+                return action
         assert target.pellet is not None
-        target_pellet: Pellet = target.pellet
-        return f"MOVE {pac.pac_id} {target_pellet.position.row} {target_pellet.position.column}" \
-               + f" go for {target_pellet.position}"
+        return f"MOVE {pac.pac_id} {target.pellet.position.x} {target.pellet.position.y}" \
+               + f" miam {target.pellet.position}"
+
+    def get_defend_action(self,
+                          pac: Pac,
+                          defend_pac: Pac) -> Optional[str]:
+        if pac.ability_cooldown == 0:
+            new_type_id: str = defend_pac.get_beating_type()
+            return f"SWITCH {pac.pac_id} {new_type_id} switch to defend against {defend_pac.pac_id}"
+        # TODO: rather find the direction that maximise the distance with defend_pac
+        delta_position: Position = defend_pac.position - pac.position
+        target_position: Position = pac.position - delta_position
+        target_position = target_position.crop_to_limits(self.grid.get_max_position())
+        return f"MOVE {pac.pac_id} {target_position.x} {target_position.y}" \
+               + f" run away from {defend_pac.pac_id}"
+
+    @staticmethod
+    def get_attack_action(pac: Pac,
+                          attack_pac: Pac) -> Optional[str]:
+        distance: int = pac.position.manhattan_distance(attack_pac.position)
+        if 1 < distance and pac.ability_cooldown == 0:
+            return f"SPEED {pac.pac_id} rush to attack {attack_pac.pac_id}"
+        if distance <= 2 or pac.speed_turns_left > 0:
+            return f"MOVE {pac.pac_id} {attack_pac.position.x} {attack_pac.position.y}" \
+                   + f" attack {attack_pac.pac_id}"
+        return None
 
 
 def main():
